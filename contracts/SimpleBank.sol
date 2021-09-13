fce0b26302f0e0bef2d9da2374b945bfaebfda4c
Michael Daigler <mdaigler2601@gmail.com>
2021-09-13 18:21:01 -0500

    updated contract

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -46,7 +46,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    receive() external payable {
+   fallback() external payable {
         revert();
     }
 
